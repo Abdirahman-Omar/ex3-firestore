159cadf6cfea03f4cdb006ea29f95dab47bb400b
Abdirahman Omar <abdirahmano217@gmail.com>
2021-03-15 00:32:40 -0400

    Dernier push et configuration firebase supprimer

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -3,15 +3,6 @@ import 'firebase/firestore';
 
 /******* Ex#3 - Étape B ********************************/ 
 // Ajouter votre objet de configuration Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyBqQRS8GaaRalAqeY3_-BLptJKi6GPMeig",
-  authDomain: "abdirahman-react.firebaseapp.com",
-  projectId: "abdirahman-react",
-  storageBucket: "abdirahman-react.appspot.com",
-  messagingSenderId: "302817357703",
-  appId: "1:302817357703:web:95a4100c653d7073d36d22",
-  measurementId: "G-E936X852T3"
-};
 
 // Initialiser Firebase
 if(!fbAppli.apps.length) {
@@ -22,3 +13,5 @@ if(!fbAppli.apps.length) {
 const bd = fbAppli.firestore();
 /******* Ex#3 - Étape C ********************************/ 
 // Exporter (par défaut) la référence à "bd" pour pouvoir l'utiliser dans le composant ListeProduits
+
+export default bd;
